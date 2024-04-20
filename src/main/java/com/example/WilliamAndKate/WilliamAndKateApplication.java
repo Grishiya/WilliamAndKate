bbfcf75c0fce38c85fca532e3360f7bddaa6d26e
Grisha <guruwolf8@gmail.com>
2024-04-21 00:40:19 +0300

    Создание проекта

---
+++ src/main/java/com/example/WilliamAndKate/WilliamAndKateApplication.java
@@ -0,0 +1,13 @@
+package com.example.WilliamAndKate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilliamAndKateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilliamAndKateApplication.class, args);
+	}
+
+}
